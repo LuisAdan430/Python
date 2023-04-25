f0b068971bc0b271210458af9fbc821428adc8dc
LuisAdan430 <luisangelcanalescalva@gmail.com>
2023-04-24 23:04:22 -0600

    primer ejercicio

---
+++ pythonProject/Actividad_one_operaciones.py
@@ -0,0 +1,19 @@
+num_one = int(input("Ingresa el primer número: "))
+num_two = int(input("Ingresar el segundo número: "))
+print(f'La suma de los números : {str(num_one)} + {str(num_two)} = {num_one + num_two}')
+print(f'La multiplicacion de los números : {str(num_one)} * {str(num_two)} = {num_one * num_two}')
+print(f'¿Los números son iguales ?  {str(num_one)} == {str(num_two)} : {num_one == num_two}')
+print(f'¿El primero numero es menor que el segundo ?  {str(num_one)} < {str(num_two)} : {num_one < num_two}')
+print(f'¿El segundo numero es mayor o igual que el primero ?  {str(num_one)} >= {str(num_two)} : {num_one >= num_two}')
+
+# Codigo de Edutin
+primer_numero = int(input("Ingresa un número (1): " ))
+segundo_numero = int(input("Ingresa un número (2): " ))
+#Operaciones aritméticas
+print(f'La suma es: {primer_numero + segundo_numero}')
+print(f'La multiplicación es: {primer_numero * segundo_numero}')
+
+#Operaciones de comparación
+print(f'¿los números son iguales?: {primer_numero == segundo_numero}')
+print(f'¿El primer número es menor que el segundo?: {primer_numero < segundo_numero}')
+print(f'¿El segundo número es mayor o igual al primero?: {segundo_numero >= primer_numero}')
---
+++ pythonProject/operadores_asignacion.py
@@ -0,0 +1,28 @@
+numero = 4
+#Flujo de programa
+
+#Instruccion 1
+#Instruccion 2
+#Instruccion 3
+#Instruccion 4
+
+#Ejemplo suma
+#numero = numero + 5
+numero += 5
+print(numero)
+#Ejemplo resta
+#numero = numero - 5
+numero -= 5
+print(numero)
+#Ejemplo multiplicacion
+#numero = numero  * 10
+numero *= 10
+print(numero)
+#Ejemplo division
+# numero = numero / 5
+numero /= 4
+print(numero)
+#Ejemplo modulo
+# numero = numero % 6
+numero %= 6
+print(numero)
