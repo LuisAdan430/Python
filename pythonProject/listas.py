4a69bbdeaff21c2f8461aa11ff136ce62e2d15b1
LuisAdan430 <luisangelcanalescalva@gmail.com>
2023-05-04 16:26:48 -0600

    listas

---
+++ pythonProject/listas.py
@@ -0,0 +1,30 @@
+'''
+Escribir un programa que almacene las asignaturas de una clase
+'''
+'''
+asingaturas = ["Matematicas","Fisica","Español","Ingles"]
+print(asingaturas[3])
+print(type(asingaturas))
+'''
+'''
+#Loteria
+numeros = []
+for i in range(6):
+    numeros.append(int(input("Introduce un numero: ")))
+numeros.sort()
+print(f'Lo numeros ganadores son:{numeros}')
+
+for i in lista:
+    print(i)
+'''
+lista = [1,4,5,6,7,8,7,"Hola"]
+
+#lista.remove(6)
+#lista.pop(5)
+#del lista[0]
+#lista.clear()
+#lista.count(6)
+#lista.index("Hola")
+#print("Esta es la lista despues de colocar el metodo clear:" +str(lista))
+lista.reverse()
+print(lista)
