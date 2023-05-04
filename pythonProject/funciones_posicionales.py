7c14f0c7a9054e0dc726622120a7e59163b0c61c
LuisAdan430 <luisangelcanalescalva@gmail.com>
2023-05-04 01:15:14 -0600

    funciones_posicionales 2/2

--- pythonProject/funciones_posicionales.py
+++ pythonProject/funciones_posicionales.py
@@ -1,7 +1,25 @@
+'''
 def suma(*args):
     s = 0
     for i in args:
         s += i
     return s
-resultado = suma(5,3,5,7,8,9)
+resultado = suma(10,10,10,10)
 print(resultado)
+def lenguaje(nombre, *lenguajes):
+    print(f'Hola {nombre}')
+    print(f'Tus lenguajes favoritos son: {lenguajes}')
+lenguaje("Luis Calva","JAVA","Python","Php")
+'''
+def lenguaje(nombre,**lenguajes):
+    print(f'Hola {nombre}')
+    print("Buscando informacion acerca de tus lenguajes favoritos...")
+    print("Cargando informacion ...\n")
+    print("Informacion: ")
+    contador = 0
+    print(type(lenguajes))
+    for clave in lenguajes:
+        contador += 1
+        print(f'Tu {contador} lenguaje favorito es: {lenguajes[clave]}')
+
+lenguaje("Luis Calva",lenguaje1= "JAVA",lenguaje2 = "Python",lenguaje3 ="Php")
