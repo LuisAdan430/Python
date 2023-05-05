d9eb2958b36c85abd978e9b6b1e3b3c04bda163e
LuisAdan430 <luisangelcanalescalva@gmail.com>
2023-05-05 16:50:47 -0600

    polimorfismo

---
+++ pythonProject/polimorfismo.py
@@ -0,0 +1,15 @@
+class Gato():
+    def sonido(self):
+        print("MIAU")
+class Perro():
+    def sonido(self):
+        print("GUAU")
+class Cerdo():
+    def sonido(self):
+        print("OING OING")
+def escucharSonido(animal):
+    animal.sonido()
+gato1 = Gato()
+perro1 = Perro()
+cerdo1 = Cerdo()
+escucharSonido(cerdo1)
